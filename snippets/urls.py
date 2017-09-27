20cb60cd63766825a4a05147f44d3fdb81721984
yurchik <15_yurchik@ukr.net>
2017-09-27 23:29:35 +0300

    Class base view with different mixins

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>\d+)/$', views.snipet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>\d+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
